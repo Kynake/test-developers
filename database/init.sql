26e0e6605173cc17b8d1115398de3febbcb810e1
Pedro Ortiz Bledow <pedro.bledow@acad.pucrs.br>
2019-04-10 22:20:36 -0300

    Updated initializing SQL script Created POST, PUT and DELETE routes

--- database/init.sql
+++ database/init.sql
@@ -1,40 +1,40 @@
 CREATE TABLE documents(
-   id INT PRIMARY KEY       NOT NULL,
-   name           TEXT      NOT NULL,
+   id SERIAL PRIMARY KEY      NOT NULL,
+   name           TEXT UNIQUE NOT NULL,
    content        TEXT,
-   timestamp      TIMESTAMP NOT NULL
+   timestamp      TIMESTAMP   NOT NULL
 );
 
 CREATE TABLE signatures(
-  id INT PRIMARY KEY       NOT NULL,
-  name           TEXT      NOT NULL,
-  issuer         TEXT      NOT NULL,
-  ordering       INT       NOT NULL,
-  timestamp      TIMESTAMP NOT NULL,
-  id_document    INT REFERENCES documents(ID)
+  id SERIAL PRIMARY KEY      NOT NULL,
+  name           TEXT UNIQUE NOT NULL,
+  issuer         TEXT        NOT NULL,
+  ordering       INT         NOT NULL,
+  timestamp      TIMESTAMP   NOT NULL,
+  id_document    INT NOT NULL REFERENCES documents(ID)
 );
 
-INSERT INTO documents VALUES (
-  '1', 'Documento de Teste', 'Lorem Ipsum', CURRENT_TIMESTAMP
+INSERT INTO documents (name, content, timestamp) VALUES (
+  'Documento de Teste', 'Lorem Ipsum', CURRENT_TIMESTAMP
 );
 
-INSERT INTO documents VALUES (
-  '2', 'Teste2', 'I Mendeley Mendeley', CURRENT_TIMESTAMP
+INSERT INTO documents (name, content, timestamp) VALUES (
+  'Teste2', 'I Mendeley Mendeley', CURRENT_TIMESTAMP
 );
 
 
-INSERT INTO signatures VALUES (
-  '1', 'Fulano da Silva', 'Empresa Certificadora 1', '1', CURRENT_TIMESTAMP, '1'
+INSERT INTO signatures (name, issuer, ordering, timestamp, id_document) VALUES (
+  'Fulano da Silva', 'Empresa Certificadora 1', '1', CURRENT_TIMESTAMP, '1'
 );
 
-INSERT INTO signatures VALUES (
-  '2', 'Beltrano da Silva', 'Empresa Certificadora 2', '2', CURRENT_TIMESTAMP, '1'
+INSERT INTO signatures (name, issuer, ordering, timestamp, id_document) VALUES (
+  'Beltrano da Silva', 'Empresa Certificadora 2', '2', CURRENT_TIMESTAMP, '1'
 );
 
-INSERT INTO signatures VALUES (
-  '3', 'Ciclano da Silva', 'Empresa Certificadora 3', '1', CURRENT_TIMESTAMP, '2'
+INSERT INTO signatures (name, issuer, ordering, timestamp, id_document) VALUES (
+ 'Ciclano da Silva', 'Empresa Certificadora 3', '1', CURRENT_TIMESTAMP, '2'
 );
 
-INSERT INTO signatures VALUES (
-  '4', 'Deltrano da Silva', 'Empresa Certificadora 4', '2', CURRENT_TIMESTAMP, '2'
+INSERT INTO signatures (name, issuer, ordering, timestamp, id_document) VALUES (
+  'Deltrano da Silva', 'Empresa Certificadora 4', '2', CURRENT_TIMESTAMP, '2'
 );
--- test-dev-back/index.php
+++ test-dev-back/index.php
@@ -4,7 +4,7 @@ use Phalcon\Loader;
 use Phalcon\Mvc\Micro;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Db\Adapter\Pdo\Postgresql as PdoPostgresql;
-use Phalcon\Db\Column as Column;
+use Phalcon\Http\Response;
 
 // Use Loader() to autoload our model
 $loader = new Loader();
@@ -45,7 +45,8 @@ $app->get(
   '/api/documents',
   function () use ($app) {
     // Operation to fetch all the documents
-    // echo "GET api documents";
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
 
     $phql = 'SELECT * FROM Docspace\Documents ORDER BY name';
     $documents = $app->modelsManager->executeQuery($phql);
@@ -60,7 +61,12 @@ $app->get(
       ];
     }
 
-    echo json_encode($data);
+    $response->setJsonContent([
+      'status' => 'OK',
+      'data'   => $data
+    ]);
+    
+    return $response;
   }
 );
 
@@ -69,10 +75,11 @@ $app->get(
   '/api/documents/{id:[0-9]+}',
   function ($id) use ($app) {
     // Operation to fetch document with id $id
-    // echo "GET api documents $id";
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
 
     $sqldocs = 'SELECT * FROM Docspace\Documents  WHERE id          = :id:';
-    $sqlsigs = 'SELECT * FROM Docspace\Signatures WHERE id_document = :id:';
+    $sqlsigs = 'SELECT * FROM Docspace\Signatures WHERE id_document = :id: ORDER BY ordering, name';
 
     $documents  = $app->modelsManager->executeQuery($sqldocs, ['id' => $id]);
     $signatures = $app->modelsManager->executeQuery($sqlsigs, ['id' => $id]);
@@ -89,10 +96,18 @@ $app->get(
     }
 
     if(count($data) > 0) {
-      echo json_encode($data[0]);
+      $response->setJsonContent([
+        'status' => 'FOUND',
+        'data'   => $data[0]
+      ]);
     } else {
-      echo json_encode(null);
+      $response->setJsonContent([
+        'status' => 'NOT-FOUND',
+        'data'   => (object) null
+      ]);
     }
+
+    return $response;
   }
 );
 
@@ -105,7 +120,8 @@ $app->get(
   '/api/signatures',
   function () use ($app) {
     // Operation to fetch all the signatures
-    // echo "GET api signatures";
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
 
     $phql = 'SELECT * FROM Docspace\Signatures ORDER BY id_document, ordering';
     $signatures = $app->modelsManager->executeQuery($phql);
@@ -122,7 +138,12 @@ $app->get(
       ];
     }
 
-    echo json_encode($data);
+    $response->setJsonContent([
+      'status' => 'OK',
+      'data'   => $data
+    ]);
+
+    return $response;
   }
 );
 
@@ -131,27 +152,13 @@ $app->get(
   '/api/signatures/{id:[0-9]+}',
   function ($id) use ($app) {
     // Operation to fetch signature with id $id
-    // echo "GET api signatures $id";
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
 
     $sqlsigs = 'SELECT * FROM Docspace\Signatures WHERE id = :id:';
     $sqldocs = 'SELECT * FROM Docspace\Documents  WHERE id = :id_document:';
 
     $signatures = $app->modelsManager->executeQuery($sqlsigs, ['id' => $id]);
-    // $documents  = $app->modelsManager->executeQuery($sqldocs, ['id' => $id]);
-
-    // echo json_encode($signatures[0]);
-
-    // $tempData = [];
-    // foreach ($documents as $doc) {
-    //   $tempData[] = [
-    //     'id'          => $sig->id,
-    //     'name'        => $sig->name,
-    //     'issuer'      => $sig->issuer,
-    //     'timestamp'   => $sig->timestamp,
-    //     'ordering'    => $sig->ordering,
-    //     'id_document' => $sig->id_document
-    //   ];
-    // }
 
     if(count($signatures) > 0) {
       $id_document = $signatures[0]->id_document;
@@ -170,40 +177,151 @@ $app->get(
         ];
       }
 
-      echo json_encode($data[0]);
+      $response->setJsonContent([
+        'status' => 'FOUND',
+        'data'   => $data[0]
+      ]);
     } else {
-      echo json_encode(null);
+      $response->setJsonContent([
+        'status' => 'NOT-FOUND',
+        'data'   => (object) null
+      ]);
     }
+
+    return $response;
   }
 );
 
 // POST signature
 $app->post(
-    '/api/signatures',
-    function () use ($app) {
-      // Operation to create a new signature
-      echo "POST api signature";
+  '/api/signatures',
+  function () use ($app) {
+    // Operation to create a new signature
+    $signature = $app->request->getJsonRawBody();
+    
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
+    
+    $sqlsig = 'INSERT INTO Docspace\Signatures (name, issuer, timestamp, ordering, id_document) VALUES (:name:, :issuer:, :timestamp:, :ordering:, :id_document:)';
+
+    $status = $app->modelsManager->executeQuery($sqlsig, [
+      'name'        => $signature->name,
+      'issuer'      => $signature->issuer,
+      'timestamp'   => $signature->timestamp,
+      'ordering'    => $signature->ordering,
+      'id_document' => $signature->id_document
+    ]);
+
+    if($status->success() === true) {
+      $response->setStatusCode(201, 'Created');
+      $signature->id = $status->getModel()->id;
+
+      $response->setJsonContent([
+        'status' => 'OK',
+        'data'   => $signature,
+      ]);
+    } else {
+      $response->setStatusCode(409, 'Conflict');
+
+      // Send errors to the client
+      $errors = [];
+
+      foreach ($status->getMessages() as $message) {
+        $errors[] = $message->getMessage();
+      }
+
+      $response->setJsonContent([
+        'status'   => 'ERROR',
+        'data' => $errors,
+      ]);
     }
+
+    return $response;
+  }
 );
 
 // PUT signature
 $app->put(
-    '/api/signatures/{id:[0-9]+}',
-    function ($id) use ($app) {
-      // Operation to update a signature with id $id
-      echo "PUT api signature $id";
+  '/api/signatures/{id:[0-9]+}',
+  function ($id) use ($app) {
+    // Operation to update a signature with id $id
+    $signature = $app->request->getJsonRawBody();
+    
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
+
+    $sqlsig = 'UPDATE Docspace\Signatures
+               SET name = :name:, issuer = :issuer:, timestamp = :timestamp:, ordering = :ordering:, id_document = :id_document:
+               WHERE id = :id:';
+
+    $status = $app->modelsManager->executeQuery($sqlsig, [
+      'name'        => $signature->name,
+      'issuer'      => $signature->issuer,
+      'timestamp'   => $signature->timestamp,
+      'ordering'    => $signature->ordering,
+      'id_document' => $signature->id_document,
+      'id'          => $signature->id
+    ]);
+
+    if($status->success() === true) {
+      $response->setStatusCode(201, 'Updated');
+
+      $response->setJsonContent([
+        'status' => 'OK',
+        'data'   => $signature,
+      ]);
+    } else {
+      $response->setStatusCode(409, 'Conflict');
+
+      $errors = [];
+
+      foreach ($status->getMessages() as $message) {
+        $errors[] = $message->getMessage();
+      }
+
+      $response->setJsonContent([
+        'status'   => 'ERROR',
+        'data' => $errors,
+      ]);
     }
+
+    return $response;
+  }
 );
 
 $app->delete(
   '/api/signatures/{id:[0-9]+}',
   function ($id) use ($app) {
     // Operation to delete a signature with id $id
-    echo "DELETE api signature $id";
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
+    
+    $sqlsig = 'DELETE FROM Docspace\Signatures WHERE id = :id:';
+    $status = $app->modelsManager->executeQuery($sqlsig, ['id' => $id]);
+
+    if ($status->success() === true) {
+      $response->setJsonContent([
+        'status' => 'OK'
+      ]);
+    } else {
+      // Change the HTTP status
+      $response->setStatusCode(409, 'Conflict');
+
+      $errors = [];
+
+      foreach ($status->getMessages() as $message) {
+          $errors[] = $message->getMessage();
+      }
+
+      $response->setJsonContent([
+        'status'   => 'ERROR',
+        'data' => $errors,
+      ]);
+    }
+
+    return $response;
   }
 );
 
 $app->handle();
-// phpinfo();
-
 ?>
