10b69ea568b6624ef0d0b75757bf94b80a03de2e
Pedro Ortiz Bledow <pedro.bledow@acad.pucrs.br>
2019-04-16 06:04:52 -0300

    Updated Edit Signature Component

--- test-dev-front/src/app/edit-signature/edit-signature.component.ts
+++ test-dev-front/src/app/edit-signature/edit-signature.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input }           from '@angular/core'
+import { Component, OnInit, Input, Output, EventEmitter }           from '@angular/core'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 import { Signature } from '../models/signature.model';
+import { SignaturesService } from '../backend/signatures.service';
+import { Response } from '../models/response.model';
 
 @Component({
   selector: 'app-edit-signature',
@@ -14,6 +16,13 @@ export class EditSignatureComponent implements OnInit {
   @Input() id_document: number
   @Input() order:      number
 
+  @Output() onCreate:  EventEmitter<Signature> = new EventEmitter<Signature>()
+  @Output() onUpdate:  EventEmitter<Signature> = new EventEmitter<Signature>()
+  @Output() onDelete:  EventEmitter<number>    = new EventEmitter<number>()
+
+  @Output() onCancel:  EventEmitter<void> = new EventEmitter<void>()
+  @Output() onEditing: EventEmitter<void> = new EventEmitter<void>()
+
   isEditing: boolean = false
   editForm: FormGroup
 
@@ -24,7 +33,8 @@ export class EditSignatureComponent implements OnInit {
 
   //Construtor
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private signaturesService:  SignaturesService
   ) {}
 
   //Métodos
@@ -47,18 +57,111 @@ export class EditSignatureComponent implements OnInit {
     })
 
     this.isEditing = !this.editForm.valid
+    if(this.isEditing) {
+      this.onEditing.emit()
+    }
 
     this.isLoading = false
+  }
+
+  //POST Method
+  async postSignature() {
+    console.log('POST API INFO EVENT')
+    this.isLoading = true
+
+    let sig: Signature = {
+      id_document: this.editForm.value.id_document,
+      issuer:      this.editForm.value.issuer,
+      name:        this.editForm.value.name,
+      ordering:    this.editForm.value.ordering,
+      timestamp:   this.editForm.value.timestamp
+    }
+
+    let response: Response = await this.signaturesService.postSignature(sig)
+
+    if(!response.hasError) {
+      this.signature = response.data
+      this.updateForm()
+
+      this.onCreate.emit(this.signature)
+    }
+
+    this.isLoading = false
+  }
+
+  //PUT Method
+  async putSignature() {
+    console.log('PUT API INFO EVENT')
+    this.isLoading = true
+
+    let sig: Signature = {
+      id:          this.signature.id,
+      id_document: this.editForm.value.id_document,
+      issuer:      this.editForm.value.issuer,
+      name:        this.editForm.value.name,
+      ordering:    this.editForm.value.ordering,
+      timestamp:   this.editForm.value.timestamp
+    }
+
+    let response: Response = await this.signaturesService.putSignature(sig)
+
+    if(!response.hasError) {
+      this.signature = response.data
+      this.updateForm()
+
+      this.onUpdate.emit(this.signature)
+    }
+
+    this.isLoading = false
+  }
+
+  //DELETE Method
+  async deleteSignature() {
+    console.log('DELETE API INFO EVENT')
+    this.isLoading = true
+
+    if(this.signature) { //DELETE on Backend
+      let response: Response = await this.signaturesService.deleteSignature(this.signature.id)
+
+      if(!response.hasError) {
+        this.onDelete.emit(this.signature.id)
+      }
+    } else { //NEW Signature, not on Backend
+      this.onDelete.emit(undefined)
+    }
+
+    this.isLoading = false
+  }
+
+  updateForm() {
+    this.editForm.setValue({
+      id_document: this.signature.id_document,
+      ordering:    this.signature.ordering,
+      name:        this.signature.name,
+      issuer:      this.signature.issuer,
+      timestamp:   this.signature.timestamp
+    })
   }
 
   toggleEdit() {
     this.isEditing = !this.isEditing
 
-    if(!this.isEditing && this.editForm.dirty) {
-      this.editForm.markAsPristine()
+    if(this.isEditing) {
+      this.onEditing.emit()
+    } else {
+      if(this.editForm.dirty) {
+        this.editForm.markAsPristine()
       
-      console.log('UPDATE API INFO EVENT')
+        if(this.signature) {
+          this.putSignature()
+        } else {
+          this.postSignature()
+        }
+      } else {
+        this.onCancel.emit()
+      }
     }
+
   }
 
 }
