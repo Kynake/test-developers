be2b100cd3f0a817743852f137c079572da8d5f4
Pedro Ortiz Bledow <pedro.bledow@acad.pucrs.br>
2019-04-16 06:05:05 -0300

    Updated Document Detail Component

--- test-dev-front/src/app/document-detail/document-detail.component.ts
+++ test-dev-front/src/app/document-detail/document-detail.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute }    from '@angular/router'
 import { Document }         from '../models/document.model'
 import { DocumentsService } from '../backend/documents.service'
 import { Response }         from '../models/response.model'
+import { Signature }        from '../models/signature.model'
 
 @Component({
   selector: 'app-document-detail',
@@ -20,6 +21,8 @@ export class DocumentDetailComponent implements OnInit {
 
   isLoading: boolean = false
   hasError:  boolean = false
+
+  isEditing: number = 0
 
   errorMessage: string
 
@@ -52,4 +55,59 @@ export class DocumentDetailComponent implements OnInit {
     this.doc.Signatures.push(undefined)
   }
 
+  //Create
+  appendToList(sig: Signature) {
+    this.doc.Signatures[this.doc.Signatures.length - 1] = sig
+
+    this.isEditing--
+  }
+
+  //Update
+  updateExisting(sig: Signature) {
+    for(let i = 0; i < this.doc.Signatures.length; i++) {
+      if(this.doc.Signatures[i].id === sig.id) {
+        this.doc.Signatures[i] = sig
+        break
+      }
+    }
+
+    this.isEditing--
+  }
+
+  //Delete
+  clearSignatureFromList(id?: number) {
+    if(id) { //Find ID in List
+      for(let i = 0; i < this.doc.Signatures.length; i++) {
+        if(this.doc.Signatures[i].id === id) {
+          this.doc.Signatures.splice(i, 1)
+          break
+        }
+      }
+    } else { //Remove Last
+      this.doc.Signatures.pop()
+    }
+
+    this.isEditing = this.isEditing == 0? 0 : this.isEditing - 1
+  }
+
+  setOrderingValue(checkUntil: number): number {
+    if(checkUntil == 0) {
+      return this.doc.Signatures[0]? this.doc.Signatures[0].ordering : 1
+    } else {
+      return this.doc.Signatures[checkUntil - 1].ordering + 1
+    }
+  }
+
+  setIsEditing() {
+    this.isEditing++
+  }
+
+  unsetIsEditing() {
+    this.isEditing--
+  }
+
+
+  testFunc() {
+    console.log(this.isEditing)
+  }
 }
